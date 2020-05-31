191e27f1f98188ba2a1fd16a2f885d31bc311f46
Jean-BaptisteTheSage <31390107+Jean-BaptisteTheSage@users.noreply.github.com>
2020-05-31 16:48:00 -0500

    Cleans up beginning imports for App and Homescreen

--- RadioHash/App.js
+++ RadioHash/App.js
@@ -1,27 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack'
--- RadioHash/src/Screens/HomeScreen.js
+++ RadioHash/src/Screens/HomeScreen.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { SafeAreaView, View, Text, ScrollView} from 'react-native'
 
 const HomeScreen = () => {
