d16ddb1d3f995c838b177e0dfe2ce2a39bebc7de
Jean-BaptisteTheSage <31390107+Jean-BaptisteTheSage@users.noreply.github.com>
2020-06-05 12:41:31 -0500

    Starts adding local state for actions

--- CryptoRadioDisrupter/Redux/actions.js
+++ CryptoRadioDisrupter/Redux/actions.js
@@ -1,9 +1,12 @@
 import MersenneTwister from '../shared/MersenneTwister'
+import { AsyncStorage } from 'react-native';
 
 export const GENERATE_CLUSTER = 'GENERATE_CLUSTER'
 export const ADD_CLUSTER = 'ADD_CLUSTER'
 export const REMOVE_CLUSTER = 'REMOVE_CLUSTER'
 export const FAIL_TO_CREATE_SEED = 'FAIL_TO_CREATE_SEED'
+
+export const FAIL_TO_FETCH_LOCAL_DATA = 'FAIL_TO_FETCH_LOCAL_DATA'
 
 export const generateCluster = cluster => (
     { type: GENERATE_CLUSTER, cluster }
@@ -19,6 +22,10 @@ export const removeCluster = seedID => (
 
 export const failCreateSeed = error => (
     { type: FAIL_TO_CREATE_SEED, error }
+)
+
+export const failLocalFetch = error => (
+    { type: FAIL_TO_FETCH_LOCAL_DATA, error }
 )
 
 export function createSeed (cluster) {
